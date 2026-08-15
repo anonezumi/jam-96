c2708b4241b0d88ce3cf5ba8543eff07d2567eb0
Nezumi <anonymouse.lily@gmail.com>
2026-08-15 16:02:44 -0700

    tower placement

--- scenes/input_overlay.gd
+++ scenes/input_overlay.gd
@@ -6,10 +6,25 @@ const HALF_WIDTH = HEX_WIDTH / 2.0
 const SHORT_EDGE = 17.0
 const HEX_HEIGHT = 70.0 - SHORT_EDGE
 const SLOPE = SHORT_EDGE / HALF_WIDTH
+const VERT_OFFSET = (HEX_HEIGHT - SHORT_EDGE) / 2.0 + SHORT_EDGE
 
 func _on_gui_input(event: InputEvent) -> void:
 	if event is InputEventMouseButton and event.button_index == MouseButton.MOUSE_BUTTON_LEFT:
-		$"../TileMap".erase_cell(get_hex_coords(get_viewport().get_mouse_position()))
+		var coords = get_hex_coords(get_viewport().get_mouse_position())
+		var cell = $"../TileMap".get_cell_tile_data(coords)
+		if cell.get_custom_data("placeable"):
+			var tower_inst = preload("res://scenes/tower.tscn").instantiate()
+			$"..".add_child(tower_inst)
+			tower_inst.position = get_center_of_hex(coords)
+
+
+func get_center_of_hex(coords: Vector2i) -> Vector2:
+	if coords.y % 2 == 1:
+		return Vector2(coords.x * HEX_WIDTH  + HEX_WIDTH,
+					   coords.y * HEX_HEIGHT + VERT_OFFSET)
+	else:
+		return Vector2(coords.x * HEX_WIDTH  + HALF_WIDTH,
+					   coords.y * HEX_HEIGHT + VERT_OFFSET)
 
 
 func get_hex_coords(pos: Vector2) -> Vector2i: # shameless hex grid code from stackoverflow
