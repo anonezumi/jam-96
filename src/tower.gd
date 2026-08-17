7d321965d80c3d4b3c9b122113613f62494a5f06
Nezumi <anonymouse.lily@gmail.com>
2026-08-17 14:42:00 -0700

    tower rotation

--- src/tower.gd
+++ src/tower.gd
@@ -7,12 +7,13 @@ extends Node2D
 @export var shot_speed_poss: float = 10.0
 @export var bullet: PackedScene
 @export var bullet_poss: PackedScene
+@export var tower_type: TowerType
 var time_since_fire = cooldown
 var possessed = false
 
 enum TowerType {
 	LIGHTNING,
-	WATER
+	WAVE
 }
 
 func _ready():
@@ -27,6 +28,8 @@ func on_change_possessed(tower: Node2D):
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	time_since_fire -= delta
+	if possessed and tower_type == TowerType.LIGHTNING:
+		point_head(get_viewport().get_mouse_position())
 	if time_since_fire <= 0:
 		if possessed:
 			time_since_fire += cooldown_poss
@@ -37,11 +40,40 @@ func _process(delta: float) -> void:
 		else:
 			var target = get_target()
 			if target != self: # target is self if none in range
+				point_head(target.position)
 				time_since_fire += cooldown
 				var bullet_inst = bullet.instantiate()
 				add_child(bullet_inst)
 				var vel = position.direction_to(target.position) * shot_speed
 				bullet_inst.set_velocity(vel)
+
+func point_head(target: Vector2):
+	var angle = position.angle_to_point(target)
+	# divide the circle into 6 slices, with one on the x-axis
+	var ind = floori((angle/PI) * 3.0)
+	var head_sprite = null
+	var flip = false
+	match ind:
+		0:
+			head_sprite = preload("res://assets/pee shoter/downleft.png")
+			flip = true
+		1:
+			head_sprite = preload("res://assets/pee shoter/down.png")
+		2:
+			head_sprite = preload("res://assets/pee shoter/downleft.png")
+		-1:
+			head_sprite = preload("res://assets/pee shoter/upleft.png")
+			flip = true
+		-2:
+			head_sprite = preload("res://assets/pee shoter/up.png")
+		-3, 3: # edge case for exactly pi - idk when it returns -pi vs pi but gonna add this
+			head_sprite = preload("res://assets/pee shoter/upleft.png")
+	var head = find_child("Head")
+	head.texture = head_sprite
+	if flip:
+		head.scale.x = -abs(head.scale.x)
+	else:
+		head.scale.x = abs(head.scale.x)
 
 func get_target() -> Node2D: # returns self if no target found
 	var closest_enemy
